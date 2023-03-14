3d2447858b469f597afd80e864a8421cca686f69
Gustavo Netto Parnoff <79487976+GustavoNParnoff@users.noreply.github.com>
2023-03-13 22:27:24 -0300

    Starting JS

---
+++ EntendendoJS/geral.js
@@ -0,0 +1,37 @@
+/*
+
+    ~declaração de uma varivavel var ou let~
+    var/let clima = "Quente" 
+    clima = "Frio" ~valor alterado da variavel~
+    console.log(clima)
+
+     ~declaração de uma varivavel const~
+    const clima = "Quente" 
+    clima = "Frio" ~valor não alterado da variavel~
+    console.log(clima)
+
+ */
+
+/*
+Block statement
+
+{
+    let x = 8
+    console.log(x)
+}
+*/
+
+/* 
+var é global e também local
+
+*/
+
+
+console.log(x) //não esta definido
+
+{
+    var x = 8
+    
+}
+
+console.log(x) //foi definido
